57613b18c847ca6c99a5dcf6281ae755e61e1589
Daniel Shelepov <Daniel Shelepov>
2017-05-27 20:32:45 -0700

    FW: enabling template nesting
    also changed suggested templating syntax from $VAR to {{VAR}}.

--- public/scripts/frame.js
+++ public/scripts/frame.js
@@ -1,17 +1,27 @@
-(function init()
-{
+(function init() {
     'use strict';
 
+    // variable name that will indicate the requested template in a page
+    /*const*/ var TEMPLATE_KEY = "{{_TEMPLATE}}";
+
+    // attribute name that will indicate that a given element is meant to populate a templated field
+    /*const*/ var META_KEY = "data-key";
+
+    // attribute name that will indicate that a given element should be replaced by a downloaded template fragment
+    /*const*/ var META_SRC = "data-src";
+
+    // how many resolve passes will be run on the final templated page
+    /*const*/ var POPULATE_PASS_COUNT = 5;
+
+    // relative url of the current page loaded
     var currentPageLoaded = null;
 
+    // replaces all instances of $search in $input with $replace
     function replaceAll(input, search, replace) {
         return input.split(search).join(replace);
     }
 
-    function reloadDocument(html) {
-        $("html")[0].innerHTML = html;
-    }
-
+    // initializes the frame
     function initialize() {
         $("html").on("click", "a", function (event) {
             function isLinkInternal(element) {
@@ -39,63 +49,133 @@
         }
     }
 
-    function loadTemplate(url, model) {
-        url = url + "?" + Math.random();
-        $.get(url, function (data) {
-            var template = data;
+    // loads the page from a given URL
+    function loadPage(url) {
+        // fully replaces currently loaded document with new markup
+        function reloadDocument(html) {
+            $("html")[0].innerHTML = html;
+        }
 
-            for (var key in model) {
-                if (model.hasOwnProperty(key)) {
-                    template = replaceAll(template, key, model[key]);
+        // callback for when page at current url has been downloaded
+        function onPageDownloaded(data) {
+            // given a collection of jQuery elements, finds the set of template fill values defined within those 
+            // elements.
+            //
+            // RETURNS: map keyed by template key name of template values with schema:
+            //  {
+            //      isIndirect: (bool) -- if true, actual fill value needs to be downloaded from source
+            //      value: (string) -- fill value;  may not be set for isIndirect
+            //      source: (string) -- local href representing where to fetch actual value from;  defined only iff 
+            //          isIndirect
+            //  }
+            function findVars(jqElems) {
+                var vars = {};
+
+                function harvestVar(index, elem) {
+                    var jqElem = $(elem);
+                    var key = jqElem.attr(META_KEY);
+                    var src = jqElem.attr(META_SRC);
+                    var isIndirect = typeof src === "string";
+
+                    vars[key] = { isIndirect: isIndirect };
+
+                    if (isIndirect) {
+                        vars[key].source = src;
+                    } else {
+                        vars[key].value = jqElem.html();
+                    }
+                }
+
+                var query = "[" + META_KEY + "]";
+
+                var jqTopLevelHits = jqElems.filter(query);
+                jqTopLevelHits.each(harvestVar);
+
+                var jqNestedHits = jqElems.find(query);
+                jqNestedHits.each(harvestVar);
+
+                return vars;
+            }
+
+            // given a map of template fill values, resolves all indirect values by downloading them, upon which will
+            // call onResolveComplete
+            function resolveIndirectVars(vars, onResolveComplete/*()*/) {
+                function loadIndirect(url, onLoadComplete/*(data)*/) {
+                    url = url + "?" + Math.random();
+                    $.get(url, function (data) {
+                        onLoadComplete(data);
+                    }).fail(function () {
+                        onLoadComplete('ERROR: resource ' + url + " not found.");
+                    });
+                }
+
+                var pendingRequests = 0;
+
+                for (var key in vars) {
+                    if (vars.hasOwnProperty(key) && vars[key].isIndirect) {
+                        if (typeof vars[key] === "undefined" || vars[key] === "") {
+                            vars[key].value = "[" + META_SRC + "] not set for key [" + key + "]; cannot download";
+                        } else {
+                            pendingRequests++;
+
+                            (function (capturedKey) {
+                                loadIndirect(vars[capturedKey].source, function (data) {
+                                    vars[capturedKey].value = data;
+                                    pendingRequests--;
+
+                                    if (pendingRequests == 0) {
+                                        onResolveComplete();
+                                    }
+                                });
+                            })(key);
+                        }
+                    }
+                }
+
+                if (pendingRequests == 0) {
+                    onResolveComplete();
                 }
             }
 
-            reloadDocument(template);
-        });
-    }
+            // given a map of templated values, loads the template and populates with available values from the map.
+            // If there is no template specified, leaves the main page untouched.
+            function finalizeLoad(model, rawPage) {
+                if (typeof model[TEMPLATE_KEY].value !== "undefined") {
+                    var template = model[TEMPLATE_KEY].value;
 
-    function loadPage(url) {
-        function findVars(jqElems) {
-            var metakey = "data-key";
-            var vars = {};
+                    for (var i = 0; i < POPULATE_PASS_COUNT; i++) {
+                        for (var key in model) {
+                            if (model.hasOwnProperty(key)) {
+                                template = replaceAll(template, key, model[key].value);
+                            }
+                        }
+                    }
 
-            function harvestVar(index, elem) {
-                var jqElem = $(elem);
-                var key = jqElem.attr(metakey);
-                var value = jqElem.html();
+                    reloadDocument(template);
+                } else {
+                    reloadDocument(rawPage);
+                };
 
-                vars[key] = value;
+                history.replaceState({}, document.title, currentPageLoaded);
             }
-            
-            var query = "[" + metakey + "]";
 
-            var jqTopLevelHits = jqElems.filter(query);
-            jqTopLevelHits.each(harvestVar);
+            var jqPage = $(data);
+            var vars = findVars(jqPage);
 
-            var jqNestedHits = jqElems.find(query);
-            jqNestedHits.each(harvestVar);
-
-            return vars;
+            resolveIndirectVars(vars, function () {
+                finalizeLoad(vars, data);
+            });
         }
 
         var urlMod = url + "?" + Math.random();
-        $.get(urlMod, function (data) {
-            var jqPage = $(data);
-
-            var templateKey = "$_TEMPLATE";
-            var vars = findVars(jqPage);
-            if (typeof vars[templateKey] !== "undefined") {
-                loadTemplate(vars[templateKey], vars);
-            } else {
-                reloadDocument(data);
-            };
-
-            history.replaceState({}, document.title, currentPageLoaded);
-        }).fail(function () {
+        $.get(urlMod, onPageDownloaded).fail(function () {
             reloadDocument('ERROR: resource ' + url + " not found.");
         });
     }
 
+    // attempts to navigate to a local page.  If it's the same page as current, does nothing.
+    //
+    // RETURNS: false is path is the sames as current page, otherwise true
     function navigate(path) {
         if (path !== currentPageLoaded) {
             loadPage(path);
@@ -106,12 +186,14 @@
         return false;
     }
 
+    // attempts to navigate to a local page, creating a new navigation history entry if successful
     function navigatePush(path) {
         if (navigate(path)) {
             history.pushState({}, path, path);
         }
     }
 
+    // attempts to navigate to a local page, replacing the current history entry if successful
     function navigateReplace(path) {
         if (navigate(path)) {
             history.replaceState({}, path, path);
@@ -120,7 +202,5 @@
 
     initialize();
 
-    // TODO: make sure styles are unloaded/reloaded correctly
-    // TODO: nested templates
     // TODO: demo
 })();
